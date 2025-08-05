7dc014fd37b9996153eb434c16722a0ea953017e
kais-1233 <kkais0786@gmail.com>
2025-08-05 23:14:47 +0530

    Updated app.py and cleaned old files

--- app.py
+++ app.py
@@ -2,6 +2,30 @@ import streamlit as st
 import pickle
 import pandas as pd
 
+
+import base64
+
+# --------------------- Set Flipkart Logo as Background ---------------------
+def set_background(image_file):
+    with open(image_file, "rb") as f:
+        data = f.read()
+    b64 = base64.b64encode(data).decode()
+    css = f"""
+    <style>
+    .stApp {{
+        background-image: url("data:image/png;base64,{b64}");
+        background-size: contain;
+        background-position: top right;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }}
+    </style>
+    """
+    st.markdown(css, unsafe_allow_html=True)
+
+# Call this function before app UI
+set_background("logos.png")  # Image file must be in the same folder
+
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
